f65a76297619a8606909f9f9729fc08341f3497d
Joshua Eze <ezejoshuac@gmail.com>
2023-09-13 16:20:10 +0100

    Setup the dbcontext

---
+++ TaskSync.Application/Context/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using TaskSync.Domain.Entities;
+
+namespace TaskSync.Application.Context;
+
+public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+    }
+
+    public virtual DbSet<Project> Projects { get; set; }
+    public virtual DbSet<Ticket> Ticket { get; set; }
+}
