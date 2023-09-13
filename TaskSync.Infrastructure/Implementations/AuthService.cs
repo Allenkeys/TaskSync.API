f2ca2736d9ca0bf24cd1b72d24fc3020a7b754f5
Joshua Eze <ezejoshuac@gmail.com>
2023-09-13 16:19:41 +0100

    Added auth services template

---
+++ TaskSync.Infrastructure/Implementations/AuthService.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using TaskSync.Domain.Dtos.Request;
+using TaskSync.Domain.Dtos.Response;
+using TaskSync.Infrastructure.Interfaces;
+
+namespace TaskSync.Infrastructure.Implementations;
+
+public class AuthService : IAuthService
+{
+    public Task<AuthResponse> LoginAsync(LoginRequest request)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IdentityResult> SignUpAsync(CreateUserRequest request)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ TaskSync.Infrastructure/Interfaces/IAuthService.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+using TaskSync.Domain.Dtos.Request;
+using TaskSync.Domain.Dtos.Response;
+
+namespace TaskSync.Infrastructure.Interfaces;
+
+public interface IAuthService
+{
+    Task<IdentityResult> SignUpAsync(CreateUserRequest request);
+    Task<AuthResponse> LoginAsync(LoginRequest request);
+}
