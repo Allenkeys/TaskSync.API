c99b31ac4eb93331e2111010e2199aa1df59f590
Joshua Eze <ezejoshuac@gmail.com>
2023-09-16 13:24:51 +0100

    Added global exception handler

---
+++ TaskSync.API/Extensions/ExceptionHandler.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using TaskSync.Infrastructure.ValidationResponse;
+
+namespace TaskSync.API.Extensions
+{
+    public static class ExceptionHandler
+    {
+        public static void ConfigureExceptionHandler(this IApplicationBuilder app)
+        {
+            app.UseExceptionHandler(error =>
+            {
+                error.Run(async context =>
+                {
+                    context.Response.StatusCode = 500;
+                    context.Response.ContentType = "application/json";
+
+                    var contextFeatures = context.Features.Get<IExceptionHandlerFeature>();
+                    if (contextFeatures != null)
+                    {
+                        string errMessage = context.Response.StatusCode == StatusCodes.Status500InternalServerError ?
+                            "Internal server error, please contact support" : contextFeatures.Error.Message;
+                        await context.Response.WriteAsync(new ErrorResponse
+                        {
+                            Message = errMessage,
+                            Success = false,
+                            Status = context.Response.StatusCode
+                        }.ToString());
+                    }
+                });
+            });
+        }
+    }
+}
---
+++ TaskSync.API/Extensions/ServiceExtensions.cs
@@ -0,0 +1,73 @@
+﻿using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using TaskSync.Application.Context;
+using TaskSync.Application.Repository;
+using TaskSync.Domain.Entities;
+using TaskSync.Infrastructure.Implementations;
+using TaskSync.Infrastructure.Interfaces;
+using TaskSync.Infrastructure.Jwt;
+
+namespace TaskSync.API.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void AddDbConnection(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddDbContext<ApplicationDbContext>(opt =>
+                opt.UseSqlite(config.GetConnectionString("DefaultConnection")));
+        }
+
+        public static IServiceCollection RegisterServices(this IServiceCollection services)
+        {
+            services.AddScoped<IAuthService, AuthService>();
+            services.AddScoped<IProjectService, ProjectService>();
+            services.AddScoped<ITicketService, TicketService>();
+            services.AddScoped<IJwtService, JwtService>();
+            services.AddScoped<IUnitOfWork, UnitOfWork<ApplicationDbContext>>();
+
+            return services;
+        }
+
+        public static void ConfigureIdentity(this IServiceCollection services)
+        {
+            services.AddIdentity<ApplicationUser, ApplicationRole>(opt =>
+            {
+                opt.Password.RequiredLength = 8;
+                opt.Password.RequireNonAlphanumeric = false;
+                opt.Password.RequireDigit = true;
+                opt.Password.RequireUppercase = true;
+                opt.User.RequireUniqueEmail = true;
+                opt.Password.RequireLowercase = false;
+            })
+            .AddEntityFrameworkStores<ApplicationDbContext>()
+            .AddDefaultTokenProviders();
+        }
+
+        public static void ConfigureJWT(this IServiceCollection services, IConfiguration configuration)
+        {
+            var jwtSettings = configuration.GetSection("JwtConfig");
+
+            services.AddAuthentication(option =>
+            {
+                option.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                option.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+                .AddJwtBearer(opt =>
+                {
+                    opt.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidIssuer = jwtSettings["Issuer"],
+                        ValidAudience = jwtSettings["Audience"],
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(jwtSettings["Key"]))
+                    };
+                });
+        }
+    }
+}
