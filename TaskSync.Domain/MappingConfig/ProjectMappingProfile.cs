acbab43df105d79d043436cc496fd34c981e2a57
Joshua Eze <ezejoshuac@gmail.com>
2023-09-16 13:28:28 +0100

    Added profile mapper for project entity

---
+++ TaskSync.Domain/MappingConfig/ProjectMappingProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using TaskSync.Domain.Dtos.Request;
+using TaskSync.Domain.Entities;
+
+namespace TaskSync.Domain.MappingConfig;
+
+public class ProjectMappingProfile : Profile
+{
+    public ProjectMappingProfile()
+    {
+        CreateMap<UpdateProjectRequest, Project>();
+    }
+}
