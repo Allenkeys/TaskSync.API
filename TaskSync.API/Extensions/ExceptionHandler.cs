4a17e44b412240ba4bbe71939b3b6d352d6abc6e
Joshua Eze <ezejoshuac@gmail.com>
2023-09-17 10:40:21 +0100

    Added global exception handling

--- TaskSync.API/Extensions/ExceptionHandler.cs
+++ TaskSync.API/Extensions/ExceptionHandler.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.EntityFrameworkCore;
 using TaskSync.Infrastructure.ValidationResponse;
 
 namespace TaskSync.API.Extensions
@@ -17,6 +18,24 @@ namespace TaskSync.API.Extensions
                     var contextFeatures = context.Features.Get<IExceptionHandlerFeature>();
                     if (contextFeatures != null)
                     {
+                        switch (contextFeatures.Error)
+                        {
+                            case InvalidOperationException:
+                            case InvalidDataException:
+                            case KeyNotFoundException:
+                            case ArgumentException:
+                                context.Response.StatusCode = StatusCodes.Status400BadRequest;
+                                break;
+                            case DbUpdateException:
+                                context.Response.StatusCode = StatusCodes.Status409Conflict;
+                                break;
+                            case UnauthorizedAccessException:
+                                context.Response.StatusCode = StatusCodes.Status403Forbidden;
+                                break;
+                            default:
+                                context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+                                break;
+                        }
                         string errMessage = context.Response.StatusCode == StatusCodes.Status500InternalServerError ?
                             "Internal server error, please contact support" : contextFeatures.Error.Message;
                         await context.Response.WriteAsync(new ErrorResponse
