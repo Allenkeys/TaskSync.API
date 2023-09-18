516912d4a538da58d363b4e3910c40d3ba5d516a
Joshua Eze <ezejoshuac@gmail.com>
2023-09-18 01:17:42 +0100

    Added background service

--- TaskSync.Infrastructure/Implementations/NotificationBackgroundService.cs
+++ TaskSync.Infrastructure/Implementations/NotificationBackgroundService.cs
@@ -1,11 +1,37 @@
 ﻿using Microsoft.Extensions.Hosting;
+using TaskSync.Infrastructure.Interfaces;
 
 namespace TaskSync.Infrastructure.Implementations;
 
-public class NotificationBackgroundService : BackgroundService
+public class NotificationBackgroundService : IHostedService, IDisposable
 {
-    protected override Task ExecuteAsync(CancellationToken stoppingToken)
+    private readonly INotificationService _notificationService;
+    private Timer _timer;
+    public NotificationBackgroundService(INotificationService notificationService)
     {
-        throw new NotImplementedException();
+        _notificationService = notificationService;
+    }
+   
+    public Task StartAsync(CancellationToken cancellationToken)
+    {
+        _timer = new Timer(Notify, null, TimeSpan.Zero, TimeSpan.FromDays(1));
+        return Task.CompletedTask;
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        _timer?.Change(Timeout.Infinite, 0);
+        return Task.CompletedTask;
+    }
+
+    private void Notify(object state)
+    {
+        _notificationService.CreateStatusNotification();
+        _notificationService.CreateDueDateNotification();
+    }
+
+    public void Dispose()
+    {
+        _timer?.Dispose();
     }
 }
--- TaskSync.Infrastructure/Implementations/NotificationService.cs
+++ TaskSync.Infrastructure/Implementations/NotificationService.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Identity;
 using TaskSync.Application.Repository;
 using TaskSync.Domain.Dtos.Request;
 using TaskSync.Domain.Entities;
+using TaskSync.Domain.Enums;
 using TaskSync.Infrastructure.Interfaces;
 using TaskSync.Infrastructure.ValidationResponse;
 
@@ -13,6 +14,7 @@ public class NotificationService : INotificationService
     private readonly IUnitOfWork _unitOfWork;
     private readonly IRepository<Notification> _noticeRepo;
     private readonly UserManager<ApplicationUser> _userManager;
+    private readonly IRepository<Ticket> _ticketRepo;
     private readonly IMapper _mapper;
 
     public NotificationService(IUnitOfWork unitOfWork, UserManager<ApplicationUser> userManager, IMapper mapper)
@@ -30,6 +32,54 @@ public class NotificationService : INotificationService
         _noticeRepo.Create(notification);
 
         return new SuccessResponse { Success = true, Data = "Created" };
+    }
+
+    public async Task CreateDueDateNotification()
+    {
+        var dueTickets = _ticketRepo.FindBy(t => t.DueDate.Equals(DateTime.Now.AddDays(-2)) 
+            && t.Notifications.Any(x => x.NotificationTypeId != NotificationType.DueDate), 
+            trackChanges: true);
+        if (dueTickets == null) return;
+
+        IList<Notification> notifications = null;
+
+        foreach(var ticket in dueTickets)
+        {
+            notifications.Add(new Notification
+            {
+                TicketId = ticket.Id,
+                Content = "Your ticket is due in 2 days",
+                Created = DateTime.Now,
+                NotificationTypeId = NotificationType.DueDate,
+                UserId = ticket.Project.User.Id,
+            });
+        }
+
+        _ticketRepo.BulkCreate(notifications);
+    }
+
+    public async Task CreateStatusNotification()
+    {
+        var dueTickets = _ticketRepo.FindBy(t => t.Status.Equals(Status.Completed)
+           && t.Notifications.Any(x => x.NotificationTypeId != NotificationType.StatusUpdate),
+           trackChanges: true);
+        if (dueTickets == null) return;
+
+        IList<Notification> notifications = null;
+
+        foreach (var ticket in dueTickets)
+        {
+            notifications.Add(new Notification
+            {
+                TicketId = ticket.Id,
+                Content = $"Congratulation your {ticket.Title} ticket is completed",
+                Created = DateTime.Now,
+                NotificationTypeId = NotificationType.StatusUpdate,
+                UserId = ticket.Project.User.Id,
+            });
+        }
+
+        _ticketRepo.BulkCreate(notifications);
     }
 
     public Task<string> SendEmailNotification(string email)
