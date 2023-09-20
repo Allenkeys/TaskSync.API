52ac7eba0e403d68f030a09f0e6a2a79e5c77ff3
Joshua Eze <ezejoshuac@gmail.com>
2023-09-20 12:10:50 +0100

    Created user not found custom exception

---
+++ TaskSync.Infrastructure/CustomExceptions/NotFoundException.cs
@@ -0,0 +1,9 @@
+﻿namespace TaskSync.Infrastructure.CustomExceptions;
+
+public abstract class NotFoundException : Exception
+{
+    public NotFoundException(string message) : base(message)
+    {
+
+    }
+}
---
+++ TaskSync.Infrastructure/CustomExceptions/UserNotFoundException.cs
@@ -0,0 +1,17 @@
+﻿namespace TaskSync.Infrastructure.CustomExceptions;
+
+public sealed class UserNotFoundException : NotFoundException
+{
+    public UserNotFoundException() : base("User not found!")
+    {
+        
+    }
+    public UserNotFoundException(string message) : base(message)
+    {
+        
+    }
+    public UserNotFoundException(object id) : base($"User with Id: {id}, not found in our servers")
+    {
+        
+    }
+}
